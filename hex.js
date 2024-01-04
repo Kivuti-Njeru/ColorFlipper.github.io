f722c434861b4d5b3b8b9f0b11589d7e56caec3f
Alexander <77789033+Kivuti-Njeru@users.noreply.github.com>
2024-01-04 10:12:42 +0300

    Add files via upload

---
+++ hex.js
@@ -0,0 +1,19 @@
+const hex = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",]
+
+const btn = document.querySelector(".main__btn")
+const text = document.querySelector(".text")
+
+btn.addEventListener("click", () =>{
+    let hexColor = "#"
+    for(let i = 0; i<=5; i++){
+        hexColor += hex[rando()]
+    }
+    document.body.style.backgroundColor = hexColor
+    text.style.color = hexColor
+    text.textContent = hexColor
+})
+
+function rando(){
+
+    return Math.floor(Math.random() * hex.length)
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+const colors = ["red", "green", "burlywood", "#f1f5f8", "gold", "grey", "purple", "pink", "orchid", "orange"]
+
+const btn = document.querySelector('.main__btn')
+const text = document.querySelector('.text')
+
+btn.addEventListener('click', function(){
+   let randomColor = Getrandom()
+
+document.body.style.backgroundColor = colors[randomColor]
+   text.textContent = colors[randomColor]
+   text.style.color = colors[randomColor]
+})
+
+
+
+function Getrandom(){
+    return Math.floor(Math.random() * colors.length) 
+}
+
+
